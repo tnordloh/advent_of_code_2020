ef4c49e3c73cebf6a1a4e0a8ccb0613a38878b46
Tim <tnordloh@gmail.com>
2020-12-17 17:50:53 -0700

    day 7!

---
+++ 07/skel.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class Skel
+  attr_reader :input
+  def initialize
+    @input = File.open('input.txt', 'r')
+  end
+
+  def parsed
+    @container ||= []
+    @parsed ||= input.each.each_with_object(Hash.new { |h, k| h[k] = [] }) do |line, acc|
+      container, contained = line.strip.gsub('.', '').gsub(/ bags?/, '').split(' contain ')
+      # p container
+      # p contained.split(/, /).map {|bag| bag.sub(/\d+ /, '') }
+      contained = contained.split(/, /).map {|bag| bag.sub(/\d+ /, '') }
+      @container << container
+
+      contained.each do |bag|
+        acc[bag] << container
+      end
+    end
+  end
+
+  def holders(bag, root_bags = [])
+    parents = parsed[bag]
+    root_bags << bag if @container.include?(bag)
+    if parents.empty?
+      root_bags
+    else
+      root_bags + parents.map { |p| holders(p, root_bags) }
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  p (Skel.new.holders("shiny gold").flatten.uniq - ["shiny gold"]).count
+end
---
+++ 07/skel_two.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class SkelTwo
+  attr_reader :input
+  def initialize
+    @input = File.open('input.txt', 'r')
+  end
+
+  def parsed
+    @container ||= []
+    @parsed ||= input.each.each_with_object(Hash.new { |h, k| h[k] = {} }) do |line, acc|
+      container, contained = line.strip.gsub('.', '').gsub(/ bags?/, '').split(' contain ')
+
+      contained.split(/, /).map {|bag|
+        number, *bag_name = bag.split(' ')
+        bag_name = bag_name.join(' ')
+        if bag_name != 'other'
+          acc[container].merge!({bag_name => number.to_i})
+        end
+      }
+    end
+  end
+
+  def how_many_bags?(bag, count)
+    return count if parsed[bag].empty?
+
+    count + parsed[bag].sum { |child, child_count|
+      count * how_many_bags?(child, child_count)
+    }
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  p SkelTwo.new.how_many_bags?("shiny gold", 1) - 1
+end
