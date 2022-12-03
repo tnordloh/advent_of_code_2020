3ce09549b1ed4b0ecf23f61bd806be4f5990d04b
Tim Nordloh <tnordloh@gmail.com>
2022-12-03 12:46:43 -0700

    update skel

--- skeleton/skel.rb
+++ skeleton/skel.rb
@@ -7,12 +7,15 @@ class Skel
   end
 
   def parsed
-    @parsed ||= input.each.reduce do |line|
-      # parse line here
-    end
+    @parsed ||= input.map(&:chomp)
+  end
+
+  def resolve
+    parsed
   end
 end
 
 if $PROGRAM_NAME == __FILE__
   skel = Skel.new
+  skel.resolve
 end
