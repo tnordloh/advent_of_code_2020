cc46c5a4f233cb0965582817d816fd066ee0ea2f
Timothy Nordloh <tim.nordloh@kin.com>
2022-12-05 11:30:48 -0700

    box moving, forever

---
+++ 2022/05_box_moving/skel_2.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class Skel
+  attr_reader :input
+  def initialize
+    @input = File.open('input.txt', 'r')
+  end
+
+  def parsed
+    @parsed ||= [:data, :steps]
+    .zip(input.map(&:chomp)
+    .chunk_while { |line| line.length != 0 }.to_a)
+    .to_h
+  end
+
+  def data
+    return @data if defined?(@data)
+    parsed[:data].pop
+    @data ||= parsed[:data].map { |line| line.ljust(parsed[:data].last.length + 1) }
+    .map { |line| line.chars }
+    .transpose
+    .select { |line| line.last != " "}
+    .map { |line| line.reverse }
+    .map { |line| line.select {|cell| cell != " "}}
+    .inject({}) { |acc, line| acc[line.shift.to_i] = line ; acc }
+  end
+
+  def steps
+    parsed[:steps].map { |step| resolve_step(step) }
+  end
+
+  def resolve_step(step)
+    /move (?<move>\d+) from (?<from>\d+) to (?<to>\d+)/.match(step)
+  end
+
+  def resolve
+    steps.each do |step|
+      data[step[:to].to_i] << data[step[:from].to_i].pop(step[:move].to_i)
+      data[step[:to].to_i].flatten!
+    end
+    data.keys.sort.map { |key| data[key].last}.join
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  skel = Skel.new
+  p skel.resolve
+end
